86502996f2cc98a641bb4dbaf6a768cd8585a518
SriramSandhya <sandhyasriram89@gmail.com>
2015-11-01 13:08:35 +0800

    Gitignore file & cleaned up NLPParserTest so that ExcelTest can use some common functions.

--- src/test/java/org/incident/monitor/bolt/NLPParserTest.java
+++ src/test/java/org/incident/monitor/bolt/NLPParserTest.java
@@ -9,7 +9,6 @@ import org.incident.bolt.NLPBolt;
 import org.incident.monitor.Incident;
 import org.junit.Test;
 
-import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import junit.framework.TestSuite;
 
@@ -26,27 +25,31 @@ public class NLPParserTest extends TestSuite {
 		nlpTest = new NLPBolt();
 	}
 
-	public List<Incident> createSimpleNameLocDate(String testString) {
-		return nlpTest.processEmailWithNLP(new Annotation(testString), this.pipeline);
-	}
-
 	public List<Incident> checkGetIncidents(String testString, String defaultDate) {
 		return nlpTest.getIncidents(testString, this.pipeline, defaultDate);
 	}
 
-	@Test
-	public void testSimpleNameLocDate() {
-		List<Incident> i = createSimpleNameLocDate("Power outage in Singapore on 20 October 2011");
-		assertEquals(1, i.size());
-		assertEquals("20 October 2011", i.get(0).getDate());
+	public void assertIncident(String expectedlocation, String expectedDate, String expectedEventName,
+			Incident actualIncident) {
+		assertEquals(expectedlocation, actualIncident.getLocation());
+		assertEquals(expectedDate, actualIncident.getDate());
+		assertEquals(expectedEventName, actualIncident.getName());
 	}
 
 	@Test
-	public void testComplexNameLocDate() {
-		List<Incident> i = createSimpleNameLocDate(
-				"Power outage in Singapore on 20 October 2011. Fire reported at North Carolina today.");
+	public void testSimpleNameLocDate() {
+		List<Incident> i = checkGetIncidents("Power outage in Singapore on 20 October 2011", "");
+		assertEquals(1, i.size());
+		assertIncident("Singapore", "20 October 2011", "Power outage", i.get(0));
+	}
+
+	@Test
+	public void testSimpleButMultipleNameLocDate() {
+		List<Incident> i = checkGetIncidents(
+				"Power outage in Singapore on 20 October 2011. Fire reported at North Carolina today.", "");
 		assertEquals(2, i.size());
-		assertEquals("North Carolina", i.get(1).getLocation());
+		assertIncident("Singapore", "20 October 2011", "Power outage", i.get(0));
+		assertIncident("North Carolina", "today", "Fire report", i.get(1));
 	}
 
 	@Test
@@ -60,36 +63,30 @@ public class NLPParserTest extends TestSuite {
 		List<Incident> i = checkGetIncidents("Power outage in Singapore on 20 October 2011", "2015-01-01");
 
 		assertEquals(1, i.size());
-		assertEquals("Singapore", i.get(0).getLocation());
-		assertEquals("20 October 2011", i.get(0).getDate());
-		assertEquals("Power outage", i.get(0).getName());
+		// assert date on the subject is used instead of default date
+		assertIncident("Singapore", "20 October 2011", "Power outage", i.get(0));
 	}
 
 	@Test
 	public void testSubjectWithNoDateWithCapitalizedWords() {
 		List<Incident> i = checkGetIncidents("Power Outage in Singapore", "2015-01-01");
 		assertEquals(1, i.size());
-		assertEquals("Singapore", i.get(0).getLocation());
-		assertEquals("2015-01-01", i.get(0).getDate());
-		assertEquals("Power Outage", i.get(0).getName());
+		assertIncident("Singapore", "2015-01-01", "Power Outage", i.get(0));
 	}
 
 	@Test
 	public void testSubjectWithNoDateWithOrganization() {
 		List<Incident> i = checkGetIncidents("Fire in Singapore at Police Department Headquarters.", "2015-01-01");
 		assertEquals(1, i.size());
-		assertEquals("Singapore Police Department Headquarters", i.get(0).getLocation());
-		assertEquals("2015-01-01", i.get(0).getDate());
-		assertEquals("Fire", i.get(0).getName());
+		// assert default date and organization after proposition are used
+		assertIncident("Singapore Police Department Headquarters", "2015-01-01", "Fire", i.get(0));
 	}
 
 	@Test
 	public void testSubjectWithNoDateNoEvent() {
 		List<Incident> i = checkGetIncidents("Flooded Singapore reported.", "2015-01-01");
 		assertEquals(1, i.size());
-		assertEquals("Singapore", i.get(0).getLocation());
-		assertEquals("2015-01-01", i.get(0).getDate());
-		assertEquals("flood report", i.get(0).getName());
+		assertIncident("Singapore", "2015-01-01", "flood report", i.get(0));
 	}
 
 	@Test
@@ -97,32 +94,31 @@ public class NLPParserTest extends TestSuite {
 		List<Incident> i = checkGetIncidents(
 				"Power outage in Singapore on 20 October 2011. Wild fire reported at North Carolina. Power outage today.",
 				"2015-01-01");
+		// assert that last sentence is not used
 		assertEquals(2, i.size());
-		assertEquals("Singapore", i.get(0).getLocation());
-		assertEquals("20 October 2011", i.get(0).getDate());
-		assertEquals("Power outage", i.get(0).getName());
-
-		assertEquals("North Carolina", i.get(1).getLocation());
-		assertEquals("2015-01-01", i.get(1).getDate());
-		assertEquals("Wild fire report", i.get(1).getName());
+		assertIncident("Singapore", "20 October 2011", "Power outage", i.get(0));
+		assertIncident("North Carolina", "2015-01-01", "Wild fire report", i.get(1));
 	}
 
 	@Test
 	public void testSubjectWithDateBeforeLocation() {
 		List<Incident> i = checkGetIncidents("Fire reported in 30 Brooklyn Avenue, America.", "2015-01-01");
 		assertEquals(1, i.size());
-		assertEquals("30 Brooklyn Avenue America", i.get(0).getLocation());
-		assertEquals("2015-01-01", i.get(0).getDate());
-		assertEquals("Fire report", i.get(0).getName());
+		assertIncident("30 Brooklyn Avenue America", "2015-01-01", "Fire report", i.get(0));
 	}
 
 	@Test
 	public void testSubjectWithNumberBeforeLocation() {
 		List<Incident> i = checkGetIncidents("Fire reported in 300 Brooklyn Avenue, America.", "2015-01-01");
 		assertEquals(1, i.size());
-		assertEquals("300 Brooklyn Avenue America", i.get(0).getLocation());
-		assertEquals("2015-01-01", i.get(0).getDate());
-		assertEquals("Fire report", i.get(0).getName());
+		assertIncident("300 Brooklyn Avenue America", "2015-01-01", "Fire report", i.get(0));
+	}
+
+	@Test
+	public void testSubjectWithNumberAfterLocation() {
+		List<Incident> i = checkGetIncidents("Fire reported in 300 Brooklyn Avenue 209332, America.", "2015-01-01");
+		assertEquals(1, i.size());
+		assertIncident("300 Brooklyn Avenue 209332 America", "2015-01-01", "Fire report", i.get(0));
 	}
 
 	@Test
@@ -138,9 +134,7 @@ public class NLPParserTest extends TestSuite {
 						+ "mounting to 2,200 for this year alone. Some other details that we don't need to parse.",
 				"");
 		assertEquals(1, i.size());
-		assertEquals("India", i.get(0).getLocation());
-		assertEquals("April 13 2015", i.get(0).getDate());
-		assertEquals("cases H1N1 report death toll mount", i.get(0).getName());
+		assertIncident("India", "April 13 2015", "cases H1N1 report death toll mount", i.get(0));
 	}
 
 }
